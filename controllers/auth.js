4262dbfd9eba377d0638fa7d15c9ada2151648a3
Sayantan <sayanbis83@gmail.com>
2022-06-10 14:35:41 +0530

    Added nodemailer for otp

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ var mysql      = require('mysql');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
+const nodemailer = require("nodemailer");
 
 dotenv.config();
 let otp = null;
@@ -87,6 +88,28 @@ exports.sendOTP = async function sendOTP(req,res) {
         // Send Mail here
         console.log(AdminEmail);
         console.log(otp);
+
+        let transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.email",
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: process.env.EMAIL_USERNAME, 
+                pass: process.env.EMAIL_PASSWORD, 
+            },
+        });
+
+        // send mail with defined transport object
+        let info = await transporter.sendMail({
+            from: `"ADMIN" <${process.env.EMAIL_USERNAME}>`, // sender address
+            to: AdminEmail, // list of receivers
+            subject: "Password reset OTP", // Subject line
+            text: `Your OTP for admin password reset is ${otp}.`
+        });
+
+        console.log("Message sent: %s", info.messageId);
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
         return res.json({"Message": "OTP sent successfully"});
 
     });
